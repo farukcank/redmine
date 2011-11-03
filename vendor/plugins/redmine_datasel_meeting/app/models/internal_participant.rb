3bfca93b9b02e2d68acc96191d3d3886bcbe367e
Faruk Can KAYA <farukcank@gmail.com>
2011-11-03 12:24:30 +0200

    Working on user selection

--- vendor/plugins/redmine_datasel_meeting/app/models/internal_participant.rb
+++ vendor/plugins/redmine_datasel_meeting/app/models/internal_participant.rb
@@ -1,4 +1,5 @@
 class InternalParticipant < ActiveRecord::Base
   belongs_to :meeting, :class_name => 'Meeting', :foreign_key => 'meeting_id'
   belongs_to :user, :class_name => 'User', :foreign_key => 'user_id'
+  validates_presence_of :meeting, :user
 end
