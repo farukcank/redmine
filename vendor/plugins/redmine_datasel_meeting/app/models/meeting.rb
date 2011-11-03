9b4d00514367d079b4dee6949edc9326eca8e7e3
Faruk Can KAYA <farukcank@gmail.com>
2011-11-03 13:11:15 +0200

    Working on user selection

--- vendor/plugins/redmine_datasel_meeting/app/models/meeting.rb
+++ vendor/plugins/redmine_datasel_meeting/app/models/meeting.rb
@@ -4,6 +4,6 @@ class Meeting < ActiveRecord::Base
   belongs_to :issue, :class_name => 'Issue', :foreign_key => 'issue_id'
   has_many :internal_participants, :class_name => 'InternalParticipant', :dependent => :delete_all
   has_many :internal_participant_users, :through => :internal_participants
-  validates_presence_of :date, :place, :subject, :project, :issue
+  validates_presence_of :date, :place, :subject, :project, :issue, :convacator
   validates_length_of :subject, :maximum => 255
 end
