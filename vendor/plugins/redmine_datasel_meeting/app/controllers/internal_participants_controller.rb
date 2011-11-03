39da1bba6f23a2cf1ce93603632bff462687c555
Faruk Can KAYA <farukcank@gmail.com>
2011-11-03 14:40:49 +0200

    Working on internal participants

--- vendor/plugins/redmine_datasel_meeting/app/controllers/internal_participants_controller.rb
+++ vendor/plugins/redmine_datasel_meeting/app/controllers/internal_participants_controller.rb
@@ -1,4 +1,20 @@
 class InternalParticipantsController < ApplicationController
   unloadable
-
+  def create
+  	@meeting = Meeting.find(params[:meeting_id])
+  	@internal_participant = InternalParticipant.new(params[:internal_participant])
+  	respond_to do |format|
+      format.html { redirect_to :controller => 'meetings', :action => 'show', :meeting_id => @meeting, :project_id => @meeting.project.id }
+      format.js do
+        @internal_participants = @meeting.internal_participants
+        render :update do |page|
+          page.replace_html "internalparticipants", :partial => 'meetings/internalparticipants'
+          if @relation.errors.empty?
+            page << "$('relation_delay').value = ''"
+            page << "$('relation_issue_to_id').value = ''"
+          end
+        end
+      end
+    end
+  end
 end
