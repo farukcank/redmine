00a36ae98cdf8eccc82770601652dea4291a64fe
Faruk Can KAYA <farukcank@gmail.com>
2011-11-04 15:44:06 +0200

    Fixed migration again.

--- vendor/plugins/redmine_datasel_meeting/db/migrate/20111104150045_meetingfix.rb
+++ vendor/plugins/redmine_datasel_meeting/db/migrate/20111104150045_meetingfix.rb
@@ -1,16 +1,5 @@
 class Meetingfix < ActiveRecord::Migration
   def self.up
-    create_table :meetings do |t|
-      t.column :subject, :string, :null => false
-      t.column :place, :string
-      t.column :date, :datetime, :null => false
-      t.column :external_participants, :string
-      t.column :agenda, :text
-      t.column :content, :text
-      t.column :issue_id, :integer, :null => false
-      t.column :convacator_id, :integer, :null => false
-      t.column :project_id, :integer, :null => false
-    end
     add_column :meetings, :status_id, :integer, :default => 0, :null => false
     add_column :meetings, :hours, :float, :null => false
     remove_column :meetings, :project_id
