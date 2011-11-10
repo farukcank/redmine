057c5590509a40c7bf530c7b9d08a6a69a70a393
Faruk Can KAYA <farukcank@gmail.com>
2011-11-10 18:36:03 +0200

    Working on internal participant invited - attended states

--- vendor/plugins/redmine_datasel_meeting/app/controllers/internal_participants_controller.rb
+++ vendor/plugins/redmine_datasel_meeting/app/controllers/internal_participants_controller.rb
@@ -4,20 +4,56 @@ class InternalParticipantsController < ApplicationController
   	@meeting = Meeting.find(params[:meeting_id])
   	@internal_participant = InternalParticipant.new(params[:internal_participant])
   	@internal_participant.meeting = @meeting
-  	if !@internal_participant.save
-  		puts 'Failed to save internal participant'
-  	end
+	@internal_participant.invited = true
+	@internal_participant.attended = true
+  	@internal_participant.save
   	respond_to do |format|
-      format.html { redirect_to :controller => 'meetings', :action => 'show', :meeting_id => @meeting, :project_id => @meeting.project.id }
-      format.js do
-        @internal_participants = @meeting.internal_participants
-        render :update do |page|
-          page.replace_html "internalparticipants", :partial => 'meetings/internal_participants', :locals => {:meeting => @meeting}
-          if @meeting.errors.empty?
-            page << "$('internal_participant_user_id').value = ''"
-          end
+      		format.html { redirect_to :controller => 'meetings', :action => 'show', :meeting_id => @meeting, :project_id => @meeting.project.id }
+      		format.js do
+        		@internal_participants = @meeting.internal_participants
+        		render :update do |page|
+          			page.replace_html "internalparticipants", :partial => 'meetings/internal_participants', :locals => {:meeting => @meeting}
+          			if @meeting.errors.empty?
+            				page << "$('internal_participant_user_id').value = ''"
+          			end
+        		end
+      		end
+    	end
+  end
+  def make_invited
+  	@meeting = Meeting.find(params[:meeting_id])
+	@internal_participant = @meeting.internal_participants.select{|p| p.id=params[:internal_participant_id]}.first
+	@internal_participant.invited = true
+	@internal_participant.save
+	respond_to do |format|
+                format.html { redirect_to :controller => 'meetings', :action => 'show', :meeting_id => @meeting, :project_id => @meeting.project.id }
+                format.js do
+                        @internal_participants = @meeting.internal_participants
+                        render :update do |page|
+                                page.replace_html "internalparticipants", :partial => 'meetings/internal_participants', :locals => {:meeting => @meeting}
+                                if @meeting.errors.empty?
+                                        page << "$('internal_participant_user_id').value = ''"
+                                end
+                        end
+                end
         end
-      end
-    end
+  end
+  def make_uninvited
+        @meeting = Meeting.find(params[:meeting_id])
+        @internal_participant = @meeting.internal_participants.select{|p| p.id=params[:internal_participant_id]}.first
+        @internal_participant.invited = false
+        @internal_participant.save
+        respond_to do |format|
+                format.html { redirect_to :controller => 'meetings', :action => 'show', :meeting_id => @meeting, :project_id => @meeting.project.id }
+                format.js do
+                        @internal_participants = @meeting.internal_participants
+                        render :update do |page|
+                                page.replace_html "internalparticipants", :partial => 'meetings/internal_participants', :locals => {:meeting => @meeting}
+                                if @meeting.errors.empty?
+                                        page << "$('internal_participant_user_id').value = ''"
+                                end
+                        end
+                end
+        end
   end
 end
--- vendor/plugins/redmine_datasel_meeting/app/controllers/meetings_controller.rb
+++ vendor/plugins/redmine_datasel_meeting/app/controllers/meetings_controller.rb
@@ -11,41 +11,45 @@ class MeetingsController < ApplicationController
   end
   
   def update
-	find_meeting
-  	#@meeting.convacator = User.find params[:meeting][:convacator_id]
-  	issue = @meeting.issue
-  	issue.subject = @meeting.subject
-  	issue.description = @meeting.agenda
-  	issue.save
-  	if @meeting.update_attributes(params[:meeting])
-  		redirect_to :action => 'show', :id => @project, :meeting_id => @meeting.id
-  	else
-  		render :action => 'edit'
-  	end
+	Meeting.transaction do
+		find_meeting
+  		#@meeting.convacator = User.find params[:meeting][:convacator_id]
+  		issue = @meeting.issue
+  		issue.subject = @meeting.subject
+  		issue.description = @meeting.agenda
+  		issue.save
+  		if @meeting.update_attributes(params[:meeting])
+  			redirect_to :action => 'show', :id => @project, :meeting_id => @meeting.id
+  		else
+  			render :action => 'edit'
+  		end
+		issue = @meeting.issue
+                issue.save!
+	end
   end
   
   def create
 	Meeting.transaction do
-  	#@meeting.convacator = User.find params[:meeting][:convacator_id]
-	issue = Issue.new :created_on => Time.now, :start_date => Time.now
-	@meeting = Meeting.new
-	@meeting.issue = issue	
-	@meeting.attributes=params[:meeting]
-  	issue.subject = @meeting.subject
-  	issue.description = @meeting.agenda
-  	issue.tracker = @project.trackers.find(:first)
-	issue.project = @project
-	issue.author = User.current
-	#issue.assigned_to_id = @meeting.convacator_id
-	issue.status = IssueStatus.default
-	issue.priority = IssuePriority.all[0]
-  	issue.save!
-  	if @meeting.save
-  		redirect_to :action => 'show', :id => @project, :meeting_id => @meeting.id
-  	else
-  		render :action => 'new'
-		raise ActiveRecord::Rollback
-  	end
+  		#@meeting.convacator = User.find params[:meeting][:convacator_id]
+		issue = Issue.new :created_on => Time.now, :start_date => Time.now
+		@meeting = Meeting.new
+		@meeting.issue = issue	
+		@meeting.attributes=params[:meeting]
+  		issue.subject = @meeting.subject
+  		issue.description = @meeting.agenda
+  		issue.tracker = @project.trackers.find(:first)
+		issue.project = @project
+		issue.author = User.current
+		#issue.assigned_to_id = @meeting.convacator_id
+		issue.status = IssueStatus.default
+		issue.priority = IssuePriority.all[0]
+  		issue.save!
+  		if @meeting.save
+  			redirect_to :action => 'show', :id => @project, :meeting_id => @meeting.id
+  		else
+  			render :action => 'new'
+			raise ActiveRecord::Rollback
+  		end
 	end
   end
   
