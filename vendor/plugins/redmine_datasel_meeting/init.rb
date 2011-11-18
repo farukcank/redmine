f35d6a3bdd9886fdba84ca36a39f16da2d4b9915
Faruk Can KAYA <farukcank@gmail.com>
2011-11-18 13:50:21 +0200

    Meeting is now a module which can be turned on or of per project

--- vendor/plugins/redmine_datasel_meeting/init.rb
+++ vendor/plugins/redmine_datasel_meeting/init.rb
@@ -8,6 +8,12 @@ Redmine::Plugin.register :redmine_datasel_meeting do
   url 'https://avicenna.datasel.com.tr/redmine/'
   author_url 'https://avicenna.datasel.com.tr/redmine/'
 
-  permission :meetings, { :meetings => [:index, :show, :new, :edit] }, :public => true
+  #permission :meetings, { :meetings => [:index, :show, :new, :edit] }, :public => true
+  project_module :meeting do
+    permission :list_meetings, :meetings => :index
+    permission :show_meeting, :meetings => :show
+    permission :new_meeting, :meetings => :new
+    permission :edit_meeting, :meetings => :edit
+  end
   menu :project_menu, :meetings, { :controller => 'meetings', :action => 'index' }, :caption => 'Meetings', :after => :activity, :param => :id
 end
