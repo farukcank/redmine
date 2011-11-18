5eea96e6cd3a070295061cbfcae6ce1220552c98
Faruk Can KAYA <farukcank@gmail.com>
2011-11-18 14:58:19 +0200

    Internal participants are now working with attended and invited status

--- vendor/plugins/redmine_datasel_meeting/app/controllers/internal_participants_controller.rb
+++ vendor/plugins/redmine_datasel_meeting/app/controllers/internal_participants_controller.rb
@@ -21,35 +21,53 @@ class InternalParticipantsController < ApplicationController
     	end
   end
   def make_invited
-  	@meeting = Meeting.find(params[:meeting_id])
-	@internal_participant = @meeting.internal_participants.select{|p| p.id=params[:internal_participant_id]}.first
-	@internal_participant.invited = true
-	@internal_participant.save
-	respond_to do |format|
-                format.html { redirect_to :controller => 'meetings', :action => 'show', :meeting_id => @meeting, :project_id => @meeting.project.id }
-                format.js do
-                        @internal_participants = @meeting.internal_participants
-                        render :update do |page|
-                                page.replace_html "internalparticipants", :partial => 'meetings/internal_participants', :locals => {:meeting => @meeting}
-                                if @meeting.errors.empty?
-                                        page << "$('internal_participant_user_id').value = ''"
-                                end
-                        end
-                end
+        process_internal_participant do |p|
+                p.invited = true
+		p.save
         end
   end
   def make_uninvited
+	process_internal_participant do |p|
+		p.invited = false
+		if p.attended
+			p.save
+		else
+			p.delete
+		end
+	end
+  end
+  def make_attended
+        process_internal_participant do |p|
+                p.attended = true
+                p.save
+        end
+  end
+  def make_not_attended
+        process_internal_participant do |p|
+                p.attended = false
+		if p.invited
+                	p.save
+		else
+			p.delete
+		end
+        end
+  end
+  private
+  def process_internal_participant
         @meeting = Meeting.find(params[:meeting_id])
-        @internal_participant = @meeting.internal_participants.select{|p| p.id=params[:internal_participant_id]}.first
-        @internal_participant.invited = false
-        @internal_participant.save
+        puts params[:internal_participant_id]
+        internal_participant = InternalParticipant.find(params[:internal_participant_id])
+        if internal_participant.meeting != @meeting
+                render_404
+        end
+	yield(internal_participant)
         respond_to do |format|
                 format.html { redirect_to :controller => 'meetings', :action => 'show', :meeting_id => @meeting, :project_id => @meeting.project.id }
                 format.js do
-                        @internal_participants = @meeting.internal_participants
+                        internal_participants = @meeting.internal_participants
                         render :update do |page|
                                 page.replace_html "internalparticipants", :partial => 'meetings/internal_participants', :locals => {:meeting => @meeting}
-                                if @meeting.errors.empty?
+                                if internal_participant.errors.empty?
                                         page << "$('internal_participant_user_id').value = ''"
                                 end
                         end
