9593568e62f8fc95f5131314eef57108e4a33831
Gokay Gok <gokaygok@gmail.com>
2011-11-22 16:12:42 +0200

    local avatar plugin added

---
+++ vendor/plugins/redmine_local_avatars_003/lib/users_avatar_patch.rb
@@ -0,0 +1,17 @@
+#require 'application'
+#require 'user' 
+
+module LocalAvatarsPlugin
+  module UsersAvatarPatch
+    def self.included(base) # :nodoc:    
+      base.class_eval do      
+        unloadable
+        acts_as_attachable
+  			has_many :attachments, {:as => :container,
+  															:order => "#{Attachment.table_name}.created_on",
+  															:dependent => :destroy}
+      end
+    end
+  end
+end
+User.send(:include, LocalAvatarsPlugin::UsersAvatarPatch)
